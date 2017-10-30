44082adb79e42273af3769b92f5a5c8b3e4bcb34
David Dassau <todieahero@hotmail.com>
2017-10-30 10:23:28 -0500

    added star rating

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,48 +1,85 @@
 "use strict";
 
+let firebaseApi = require('./firebaseApi');
+
 const domString = (movieArray, imgConfig, divName, search) => {
-	console.log("divName", divName);
-	console.log("imgConfig", imgConfig);
-	console.log("movieArray", movieArray);
-	let domStrang = '';
-	for (let i = 0; i < movieArray.length; i++) {
-		if (i % 3 === 0) {
-			domStrang += `<div class="row">`;
-		}
-		domStrang += `<div class="col-sm-6 col-md-4 movie">`;
-		domStrang +=   `<div class="thumbnail">`;
+    let domString = "";
+    for (let i = 0; i < movieArray.length; i++) {
+        if (i % 3 === 0) {
+            domString += `<div class="row">`;
+        }
+        domString += `<div class="col-sm-6 col-md-4 movie">`;
+        domString += `<div class="thumbnail">`;
 
-		if(!search){
-			domStrang +=     `<button class="btn btn-default delete" data-firebase-id="${movieArray[i].id}">X</button>`;
-		}
+        if (!search) {
+            domString += `<button class="btn btn-default delete" data-firebase-id="${movieArray[i].id}">X</button>`;
+        }
 
-		domStrang +=     `<img src="" alt="">`;
-		domStrang +=     `<div class="caption">`;
-		domStrang +=       `<img class="poster_path" src="${imgConfig.base_url}w342/${movieArray[i].poster_path}">`;
-		domStrang +=       `<h3 class="title">${movieArray[i].title}</h3>`;
-		domStrang +=       `<p class="overview">${movieArray[i].overview}</p>`;
-		if(search){
-			domStrang +=       `<p><a class="btn btn-primary review" role="button">Review</a> <a class="btn btn-default wishlist" role="button">Wishlist</a></p>`;
-		} else {
-			domStrang += `<p>Rating: ${movieArray[i].rating}</p>`;
-		}
-		
-		domStrang +=     `</div>`;
-		domStrang +=   `</div>`;
-		domStrang += `</div>`;
-		if (i % 3 === 2 || i === movieArray.length -1) {
-			domStrang += `</div>`;
-		}
-	}
-	printToDom(domStrang, divName);
+        domString += `<img class="poster_path" src="${imgConfig.base_url}/w342/${movieArray[i].poster_path}" alt="">`;
+        domString += `<div class="caption">`;
+        domString += `<h3 class="title">${movieArray[i].title}</h3>`;
+        domString += `<p class="overview">${movieArray[i].overview}</p>`;
+        if (search) {
+            domString += `<p>`;
+            domString += `<a class="btn btn-primary review" role="button">Review</a>`;
+            domString += `<a class="btn btn-default wishlist" role="button">Wishlist</a>`;
+            domString += `</p>`;
+        } else {
+            domString += `<label for="stars_${movieArray[i].id}" class="control-label">Rate This</label>`;
+            domString += `<input id="stars_${movieArray[i].id}" name="stars_${movieArray[i].id}" class="stars rating-loading" value="${movieArray[i].rating}">`;
+        }
+
+        domString += `</div>`;
+        domString += `</div>`;
+        domString += `</div>`;
+        if (i % 3 === 2 || i === movieArray.length - 1) {
+            domString += `</div>`;
+        }
+    }
+
+    if (!search) {
+        printToDom(domString, divName, movieArray);
+    } else {
+        printToDom(domString, divName);
+    }
+
 };
 
-const printToDom = (strang, divName) => {
-	$(`#${divName}`).append(strang);
+const initializeStars = (starArray) => {
+    starArray.forEach((star) => {
+        $("#stars_" + star.id).rating().on("rating.change", function(e, value, caption) {
+            let mommy = e.target.closest('.movie');
+            let movieId = $(e.target.closest('.thumbnail')).find('.delete').data('firebase-id');
+            let modifiedMovie = {
+                "title": $(mommy).find('.title').html(),
+                "overview": $(mommy).find('.overview').html(),
+                "poster_path": $(mommy).find('.poster_path').attr('src').split('/').pop(),
+                "rating": value,
+                "isWatched": false,
+                "uid": ""
+            };
+            firebaseApi.editMovie(modifiedMovie, movieId).then((results) => {
+                console.log(results);
+            }).catch((err) => {
+                console.log("editMovie error", err);
+            });
+        });
+    });
+};
+
+const printToDom = (strang, divName, starArray) => {
+    $(`#${divName}`).append(strang);
+
+    if (starArray) {
+        initializeStars(starArray);
+    }
 };
 
 const clearDom = (divName) => {
-	$(`#${divName}`).html('');
+    $(`#${divName}`).empty();
 };
 
-module.exports = {domString, clearDom};
+module.exports = {
+    domString,
+    clearDom
+};
